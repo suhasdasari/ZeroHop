ec6a7c5638157d16cc2136328f7ddd911e740515
Suhas Dasari <suhas.dasari194@gmail.com>
2026-02-01 00:46:40 -0500

    fixed missing explicit lightningcss-linux dep and clean next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -9,11 +9,7 @@ const nextConfig: NextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+
 };
 
 export default nextConfig;
